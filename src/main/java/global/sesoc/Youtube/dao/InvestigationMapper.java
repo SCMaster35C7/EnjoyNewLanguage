5d55c50c7d933ac40d1f12fa58655f626cdc2baf
Woo Seok <gkdltjr@naver.com>
2018-09-05 07:43:35 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/global/sesoc/Youtube/dao/InvestigationMapper.java
+++ src/main/java/global/sesoc/Youtube/dao/InvestigationMapper.java
@@ -9,7 +9,6 @@ import global.sesoc.Youtube.dto.Investigation;
 import global.sesoc.Youtube.dto.Reply;
 
 public interface InvestigationMapper {
-	
 	public int getTotalCount(Map<String, String> map);									// 자막 요청 게시글 총 수
 	public List<Investigation> selectInvList(Map<String, String> map, RowBounds bound);	// 자막 요청 게시글 가져오기
 	public int insertInvestigation(Investigation inv);									// 자막 요청 게시글 등록
@@ -21,5 +20,4 @@ public interface InvestigationMapper {
 	public int insertReplyToInv(Reply reply);											// 자막 요청 게시글 댓글 추가	
 	public int replyInvUpdate(Reply reply);												// 자막 요청 게시글 댓글 수정
 	public int replyInvDelete(int replynum);											// 자막 요청 게시글 댓글 삭제
-	
 }
--- src/main/java/global/sesoc/Youtube/dao/MemberMapper.java
+++ src/main/java/global/sesoc/Youtube/dao/MemberMapper.java
@@ -9,15 +9,10 @@ import global.sesoc.Youtube.dto.Video;
 
 public interface MemberMapper {
 	public Member selectOneFromMember(Member member); 			// 아이디 중복 검사, 아이디 검사
-
 	public int insertMember(Member member);
-
 	public int updateStatus(String useremail);
-
 	public Member selectByNick(String usernick);
-
 	public int updateLastAccess(String useremail);
-
 	public int updateMember(Map<String, String> map); 			// 회원정보 수정
 
   public Member selectMyInfo(Member member); //마이페이지 처음 정보
