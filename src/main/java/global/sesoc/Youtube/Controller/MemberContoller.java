57a901418cda25d01a2656d659b2a67bbde956ad
Unknown <1lsummer99@gmail.com>
2018-09-26 20:20:27 +0900

    합체

--- src/main/java/global/sesoc/Youtube/Controller/MemberContoller.java
+++ src/main/java/global/sesoc/Youtube/Controller/MemberContoller.java
@@ -219,7 +219,7 @@ public class MemberContoller {
          messageHelper.setTo(member.getUseremail()); // 받는사람 이메일
          messageHelper.setSubject("[앤죠애캉] 회원가입 인증"); // 메일제목은 생략이 가능하다
 
-         String host = "http://203.233.199.132:9999/Youtube/certification"; // 인증완료페이지
+         String host = "https://enjoylanguage.ml/Youtube/certification"; // 인증완료페이지
 
          messageHelper.setText("", " <h2>앤죠애캉 회원가입 인증</h2><br/>" + "<h4>인증하시려면 아래 버튼을 누르세여</h4><br/>" + "<a href="
                + host + ">"
@@ -257,7 +257,7 @@ public class MemberContoller {
          messageHelper.setTo(recoveryEmail); // 받는사람 이메일
          messageHelper.setSubject("[앤죠애캉] 회원복구 인증"); // 메일제목은 생략이 가능하다
 
-         String host = "http://203.233.199.132:9999/Youtube/recoveryID"; // 인증완료페이지
+         String host = "https://enjoylanguage.ml/Youtube/recoveryID"; // 인증완료페이지
 
          messageHelper.setText("", " <h2>앤죠애캉 회원복구 인증</h2><br/>" + "<h4>인증하시려면 아래 버튼을 누르세여</h4><br/>" + "<a href="
                + host + ">"
@@ -467,7 +467,7 @@ public class MemberContoller {
          messageHelper.setTo(member.getUseremail()); // 받는사람 이메일
          messageHelper.setSubject("[앤죠애캉] 회원가입 인증"); // 메일제목은 생략이 가능하다
 
-         String host = "http://203.233.199.132:9999/Youtube/certification"; // 인증완료페이지
+         String host = "https://enjoylanguage.ml/Youtube/certification"; // 인증완료페이지
 
          messageHelper.setText("", " <h2>앤죠애캉 회원가입 인증</h2><br/>" + "<h4>인증하시려면 아래 버튼을 누르세여</h4><br/>" + "<a href="
                + host + ">"
